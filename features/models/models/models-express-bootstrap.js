9c87f5e1cbca4015fd23662b54d42b63e043e4e6
Xavier Boubert <xavier@boubert.fr>
2016-08-26 10:08:46 +0200

    fix(models express bootstrap): require $AbstractService at start

--- features/models/models/models-express-bootstrap.js
+++ features/models/models/models-express-bootstrap.js
@@ -3,6 +3,7 @@
 var path = require('path');
 
 require(path.resolve(__dirname, 'models-database-service-back.js'))();
+require(path.resolve(__dirname, 'models-abstract-service.js'))();
 
 module.exports = function($DatabaseService, $done) {
 
