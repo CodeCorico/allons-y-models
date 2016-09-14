88b788776e9ec953065e10811ff060f56ab743d1
Xavier Boubert <xavier@boubert.fr>
2016-09-14 11:34:17 +0200

    refactor(models express bootstrap): move code logic inside the module function

--- features/models/models-40-express-bootstrap.js
+++ features/models/models-40-express-bootstrap.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var path = require('path');
+module.exports = function($processIndex, $done) {
+  var path = require('path');
 
-require(path.resolve(__dirname, 'models/models-database-service-back.js'))();
-require(path.resolve(__dirname, 'models/models-abstract-service.js'))();
+  require(path.resolve(__dirname, 'models/models-database-service-back.js'))();
+  require(path.resolve(__dirname, 'models/models-abstract-service.js'))();
 
-module.exports = function($DatabaseService, $processIndex, $done) {
+  var $DatabaseService = DependencyInjection.injector.controller.get('$DatabaseService');
 
   $DatabaseService.initModels($processIndex === 0, $done);
-
 };
