130e176bbbcf08bc71cb12aa25a5460dcdec8a88
Xavier Boubert <xavier@boubert.fr>
2016-10-31 19:21:17 +0100

    fix(models allons-y bootstrap): add before-init & after-init files to the watcher

--- features/models/models-allons-y-bootstrap.js
+++ features/models/models-allons-y-bootstrap.js
@@ -6,7 +6,7 @@ module.exports = {
       return $done();
     }
 
-    $allonsy.watcher('Allons-y Express', 'models/*-@(model|factory|factory-back|service|service-back).js');
+    $allonsy.watcher('Allons-y Express', 'models/*-@(model|factory|factory-back|service|service-back|after-init|before-init).js');
 
     $done();
   }
